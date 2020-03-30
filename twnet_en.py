a0b349a65bf8f3204f3a38d8201f8c36f553e10e
Yeh Hao-Tien <yehaotian@Iberica.local>
2020-03-30 10:22:13 +0200

    updated log

--- twnet_en.py
+++ twnet_en.py
@@ -13,7 +13,7 @@ import utils
 train_dir = './TWEETS/CLEAN/EN_CLARIN_full/train'
 dev_dir = './TWEETS/CLEAN/EN_CLARIN_full/dev'
 test_dir = './TWEETS/CLEAN/EN_CLARIN_full/test'
-de_test_dir = './TWEETS/CLEAN/DE10k_full'
+de_test_dir = './TWEETS/CLEAN/DE_CLARIN_full/test'
 train_texts, train_labels = utils.load_data(train_dir)
 dev_texts, dev_labels = utils.load_data(dev_dir)
 test_texts, test_labels = utils.load_data(test_dir)
@@ -74,22 +74,32 @@ print(x_train[:3])
 print(x_test2[:3])
 
 # load pre-trained embeddings (specify the embedding dimension)
-# embeddings_index = utils.load_embs_2_dict('EMBEDDINGS/EN_DE.txt.w2v')
+embeddings_index = utils.load_embs_2_dict('EMBEDDINGS/EN_DE.txt.w2v')
 # embeddings_index = utils.load_embs_2_dict('EMBEDDINGS/crosslingual_EN-DE_english_twitter_100d_weighted.txt.w2v')
 # embeddings_index = utils.load_embs_2_dict('EMBEDDINGS/crosslingual_EN-DE_german_twitter_100d_weighted.txt.w2v')
 
-# embedding_matrix = utils.build_emb_matrix(num_embedding_vocab=vocab_size, embedding_dim=EMBEDDING_DIM, word_index=tokenizer.word_index, embeddings_index=embeddings_index)
+embedding_matrix = utils.build_emb_matrix(num_embedding_vocab=vocab_size, embedding_dim=EMBEDDING_DIM, word_index=tokenizer.word_index, embeddings_index=embeddings_index)
 
 # build model
 model = models.Sequential()
-model.add(layers.Embedding(vocab_size, EMBEDDING_DIM, input_length=MAXLEN))
-# model.add(layers.Embedding(vocab_size, EMBEDDING_DIM, weights=[embedding_matrix], trainable=False, input_length=MAXLEN))
+# model.add(layers.Embedding(vocab_size, EMBEDDING_DIM, input_length=MAXLEN))
+model.add(layers.Embedding(vocab_size, EMBEDDING_DIM, weights=[embedding_matrix], trainable=True, input_length=MAXLEN))
 # model.add(layers.Conv1D(128, 2, padding='same', activation='relu'))
 # model.add(layers.MaxPooling1D(2))
-model.add(layers.Bidirectional(layers.LSTM(128)))
-model.add(layers.Dropout(0.2))
-model.add(layers.Dense(64, activation='relu'))
-model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Bidirectional(layers.LSTM(150)))
+model.add(layers.Dropout(0.4))
+model.add(layers.Dense(32, activation='relu'))
+model.add(layers.Dense(32, activation='relu'))
+model.add(layers.Dense(32, activation='relu'))
+model.add(layers.Dense(32, activation='relu'))
+model.add(layers.Dense(32, activation='relu'))
+model.add(layers.Dense(32, activation='relu'))
+model.add(layers.Dense(32, activation='relu'))
+model.add(layers.Dense(32, activation='relu'))
+model.add(layers.Dense(32, activation='relu'))
+model.add(layers.Dense(32, activation='relu'))
+model.add(layers.Dense(32, activation='relu'))
+model.add(layers.Dense(32, activation='relu'))
 model.add(layers.Dense(3, activation='softmax'))
 model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['acc'])
 es = EarlyStopping(monitor='val_loss', mode='auto', min_delta=0, patience=5, restore_best_weights=True, verbose=1)
