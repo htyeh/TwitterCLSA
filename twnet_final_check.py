ea43e54bbda456baa803a71c69d7651ce5269cc5
Yeh Hao-Tien <yehaotian@Iberica.local>
2020-04-01 17:28:17 +0200

    added classical experiments

--- twnet_final_check.py
+++ twnet_final_check.py
@@ -15,14 +15,14 @@ import utils
 # train_dir = './TWEETS/CLEAN/EN_CLARIN_full/train'
 # dev_dir = './TWEETS/CLEAN/EN_CLARIN_full/dev'
 test_dir = './TWEETS/CLEAN/EN_CLARIN_full/test'
-de_train_dir = './TWEETS/CLEAN/DE_CLARIN_full/train'
-de_dev_dir = './TWEETS/CLEAN/DE_CLARIN_full/dev'
+# de_train_dir = './TWEETS/CLEAN/DE_CLARIN_full/train'
+# de_dev_dir = './TWEETS/CLEAN/DE_CLARIN_full/dev'
 de_test_dir = './TWEETS/CLEAN/DE_CLARIN_full/test'
 # train_texts, train_labels = utils.load_data(train_dir)
 # dev_texts, dev_labels = utils.load_data(dev_dir)
 test_texts, test_labels = utils.load_data(test_dir)
-de_train_texts, de_train_labels = utils.load_data(de_train_dir)
-de_dev_texts, de_dev_labels = utils.load_data(de_dev_dir)
+# de_train_texts, de_train_labels = utils.load_data(de_train_dir)
+# de_dev_texts, de_dev_labels = utils.load_data(de_dev_dir)
 de_test_texts, de_test_labels = utils.load_data(de_test_dir)
 
 # MAX_WORDS = 30000
@@ -38,21 +38,21 @@ print('unique tokens in tokenizer: ' + str(vocab_size - 1))
 # train_sequences = tokenizer.texts_to_sequences(train_texts)
 # dev_sequences = tokenizer.texts_to_sequences(dev_texts)
 test_sequences = tokenizer.texts_to_sequences(test_texts)
-de_train_sequences = tokenizer.texts_to_sequences(de_train_texts)
-de_dev_sequences = tokenizer.texts_to_sequences(de_dev_texts)
+# de_train_sequences = tokenizer.texts_to_sequences(de_train_texts)
+# de_dev_sequences = tokenizer.texts_to_sequences(de_dev_texts)
 de_test_sequences = tokenizer.texts_to_sequences(de_test_texts)
 print('padding to ' + str(MAXLEN) + ' words each...')
 # train_data = pad_sequences(train_sequences, maxlen=MAXLEN)
 # dev_data = pad_sequences(dev_sequences, maxlen=MAXLEN)
 test_data = pad_sequences(test_sequences, maxlen=MAXLEN)
-de_train_data = pad_sequences(de_train_sequences, maxlen=MAXLEN)
-de_dev_data = pad_sequences(de_dev_sequences, maxlen=MAXLEN)
+# de_train_data = pad_sequences(de_train_sequences, maxlen=MAXLEN)
+# de_dev_data = pad_sequences(de_dev_sequences, maxlen=MAXLEN)
 de_test_data = pad_sequences(de_test_sequences, maxlen=MAXLEN)
 # train_labels = np.asarray(train_labels)
 # dev_labels = np.asarray(dev_labels)
 test_labels = np.asarray(test_labels)
-de_train_labels = np.asarray(de_train_labels)
-de_dev_labels = np.asarray(de_dev_labels)
+# de_train_labels = np.asarray(de_train_labels)
+# de_dev_labels = np.asarray(de_dev_labels)
 de_test_labels = np.asarray(de_test_labels)
 
 # print('en train data tensor shape = ', train_data.shape)
@@ -61,31 +61,31 @@ de_test_labels = np.asarray(de_test_labels)
 # print('en dev label tensor shape = ', dev_labels.shape)
 print('en test data tensor shape = ', test_data.shape)
 print('en test label tensor shape = ', test_labels.shape)
-print('de train data tensor shape = ', de_train_data.shape)
-print('de train label tensor shape = ', de_train_labels.shape)
-print('de dev data tensor shape = ', de_dev_data.shape)
-print('de dev label tensor shape = ', de_dev_labels.shape)
+# print('de train data tensor shape = ', de_train_data.shape)
+# print('de train label tensor shape = ', de_train_labels.shape)
+# print('de dev data tensor shape = ', de_dev_data.shape)
+# print('de dev label tensor shape = ', de_dev_labels.shape)
 print('de test data tensor shape = ', de_test_data.shape)
 print('de test label tensor shape = ', de_test_labels.shape)
 
 # train_data, train_labels = utils.shuffle(train_data, train_labels)
 # dev_data, dev_labels = utils.shuffle(dev_data, dev_labels)
 test_data, test_labels = utils.shuffle(test_data, test_labels)
-de_train_data, de_train_labels = utils.shuffle(de_train_data, de_train_labels)
-de_dev_data, de_dev_labels = utils.shuffle(de_dev_data, de_dev_labels)
+# de_train_data, de_train_labels = utils.shuffle(de_train_data, de_train_labels)
+# de_dev_data, de_dev_labels = utils.shuffle(de_dev_data, de_dev_labels)
 de_test_data, de_test_labels = utils.shuffle(de_test_data, de_test_labels)
 
-x_train_de = de_train_data
-y_train_de = de_train_labels
-x_val_de = de_dev_data
-y_val_de = de_dev_labels
+# x_train_de = de_train_data
+# y_train_de = de_train_labels
+# x_val_de = de_dev_data
+# y_val_de = de_dev_labels
 x_test_en = test_data
 y_test_en = test_labels
 x_test_de = de_test_data
 y_test_de = de_test_labels
 
 # tests
-print(x_train_de[:3])
+print(x_test_en[:3])
 print(x_test_de[:3])
 
 EMBEDDING_DIM = 100
@@ -99,9 +99,9 @@ EMBEDDING_DIM = 100
 # build model
 model = models.load_model('best_model.h5')
 print(model.summary())
-es = EarlyStopping(monitor='val_loss', mode='auto', min_delta=0, patience=5, restore_best_weights=True, verbose=1)
-mc = ModelCheckpoint('best_model.h5', monitor='val_loss', mode='auto', verbose=1, save_best_only=True)
-history = model.fit(x_train_de, y_train_de, validation_data=(x_val_de, y_val_de), batch_size=64, epochs=100, shuffle=True, callbacks=[es, mc])
+# es = EarlyStopping(monitor='val_loss', mode='auto', min_delta=0, patience=5, restore_best_weights=True, verbose=1)
+# mc = ModelCheckpoint('best_model.h5', monitor='val_loss', mode='auto', verbose=1, save_best_only=True)
+# history = model.fit(x_train_de, y_train_de, validation_data=(x_val_de, y_val_de), batch_size=64, epochs=100, shuffle=True, callbacks=[es, mc])
 print('trained embedding shape:', model.layers[0].get_weights()[0].shape)
 
 # test_loss, test_acc = model.evaluate(x_test, y_test)
--- twnet_mono.py
+++ twnet_mono.py
@@ -12,15 +12,15 @@ from sklearn.metrics import f1_score
 import pickle, json
 import utils
 
-train_dir = './TWEETS/CLEAN/EN_CLARIN_full/train'
-dev_dir = './TWEETS/CLEAN/EN_CLARIN_full/dev'
-test_dir = './TWEETS/CLEAN/EN_CLARIN_full/test'
+# train_dir = './TWEETS/CLEAN/EN_CLARIN_full/train'
+# dev_dir = './TWEETS/CLEAN/EN_CLARIN_full/dev'
+# test_dir = './TWEETS/CLEAN/EN_CLARIN_full/test'
 de_train_dir = './TWEETS/CLEAN/DE_CLARIN_full/train'
 de_dev_dir = './TWEETS/CLEAN/DE_CLARIN_full/dev'
 de_test_dir = './TWEETS/CLEAN/DE_CLARIN_full/test'
-train_texts, train_labels = utils.load_data(train_dir)
-dev_texts, dev_labels = utils.load_data(dev_dir)
-test_texts, test_labels = utils.load_data(test_dir)
+# train_texts, train_labels = utils.load_data(train_dir)
+# dev_texts, dev_labels = utils.load_data(dev_dir)
+# test_texts, test_labels = utils.load_data(test_dir)
 de_train_texts, de_train_labels = utils.load_data(de_train_dir)
 de_dev_texts, de_dev_labels = utils.load_data(de_dev_dir)
 de_test_texts, de_test_labels = utils.load_data(de_test_dir)
@@ -28,45 +28,40 @@ de_test_texts, de_test_labels = utils.load_data(de_test_dir)
 # MAX_WORDS = 30000
 MAXLEN = 30    # max tweet word count
 
+print('transforming into vectors...')
 tokenizer = Tokenizer()
-tokenizer.fit_on_texts(train_texts + dev_texts + test_texts + de_train_texts + de_dev_texts + de_test_texts)
-with open('tokenizer.pickle', 'wb') as tokenizer_output:
-    pickle.dump(tokenizer, tokenizer_output, protocol=pickle.HIGHEST_PROTOCOL)
-print('Tokenizer object exported')
-# tokenizer_json = tokenizer.to_json()
-# with open('tokenizer.json', 'w') as dumpfile:
-    # json.dump(tokenizer_json, dumpfile)
+tokenizer.fit_on_texts(de_train_texts + de_dev_texts + de_test_texts)
+# with open('tokenizer.pickle', 'wb') as tokenizer_output:
+    # pickle.dump(tokenizer, tokenizer_output, protocol=pickle.HIGHEST_PROTOCOL)
 
 vocab_size = len(tokenizer.word_index) + 1  # +UNK
 print('unique tokens in tokenizer: ' + str(vocab_size - 1))
-
-print('transforming into vectors...')
-train_sequences = tokenizer.texts_to_sequences(train_texts)
-dev_sequences = tokenizer.texts_to_sequences(dev_texts)
-test_sequences = tokenizer.texts_to_sequences(test_texts)
+# train_sequences = tokenizer.texts_to_sequences(train_texts)
+# dev_sequences = tokenizer.texts_to_sequences(dev_texts)
+# test_sequences = tokenizer.texts_to_sequences(test_texts)
 de_train_sequences = tokenizer.texts_to_sequences(de_train_texts)
 de_dev_sequences = tokenizer.texts_to_sequences(de_dev_texts)
 de_test_sequences = tokenizer.texts_to_sequences(de_test_texts)
 print('padding to ' + str(MAXLEN) + ' words each...')
-train_data = pad_sequences(train_sequences, maxlen=MAXLEN)
-dev_data = pad_sequences(dev_sequences, maxlen=MAXLEN)
-test_data = pad_sequences(test_sequences, maxlen=MAXLEN)
+# train_data = pad_sequences(train_sequences, maxlen=MAXLEN)
+# dev_data = pad_sequences(dev_sequences, maxlen=MAXLEN)
+# test_data = pad_sequences(test_sequences, maxlen=MAXLEN)
 de_train_data = pad_sequences(de_train_sequences, maxlen=MAXLEN)
 de_dev_data = pad_sequences(de_dev_sequences, maxlen=MAXLEN)
 de_test_data = pad_sequences(de_test_sequences, maxlen=MAXLEN)
-train_labels = np.asarray(train_labels)
-dev_labels = np.asarray(dev_labels)
-test_labels = np.asarray(test_labels)
+# train_labels = np.asarray(train_labels)
+# dev_labels = np.asarray(dev_labels)
+# test_labels = np.asarray(test_labels)
 de_train_labels = np.asarray(de_train_labels)
 de_dev_labels = np.asarray(de_dev_labels)
 de_test_labels = np.asarray(de_test_labels)
 
-print('en train data tensor shape = ', train_data.shape)
-print('en train label tensor shape = ', train_labels.shape)
-print('en dev data tensor shape = ', dev_data.shape)
-print('en dev label tensor shape = ', dev_labels.shape)
-print('en test data tensor shape = ', test_data.shape)
-print('en test label tensor shape = ', test_labels.shape)
+# print('en train data tensor shape = ', train_data.shape)
+# print('en train label tensor shape = ', train_labels.shape)
+# print('en dev data tensor shape = ', dev_data.shape)
+# print('en dev label tensor shape = ', dev_labels.shape)
+# print('en test data tensor shape = ', test_data.shape)
+# print('en test label tensor shape = ', test_labels.shape)
 print('de train data tensor shape = ', de_train_data.shape)
 print('de train label tensor shape = ', de_train_labels.shape)
 print('de dev data tensor shape = ', de_dev_data.shape)
@@ -74,25 +69,22 @@ print('de dev label tensor shape = ', de_dev_labels.shape)
 print('de test data tensor shape = ', de_test_data.shape)
 print('de test label tensor shape = ', de_test_labels.shape)
 
-train_data, train_labels = utils.shuffle(train_data, train_labels)
-dev_data, dev_labels = utils.shuffle(dev_data, dev_labels)
-test_data, test_labels = utils.shuffle(test_data, test_labels)
+# train_data, train_labels = utils.shuffle(train_data, train_labels)
+# dev_data, dev_labels = utils.shuffle(dev_data, dev_labels)
+# test_data, test_labels = utils.shuffle(test_data, test_labels)
 de_train_data, de_train_labels = utils.shuffle(de_train_data, de_train_labels)
 de_dev_data, de_dev_labels = utils.shuffle(de_dev_data, de_dev_labels)
 de_test_data, de_test_labels = utils.shuffle(de_test_data, de_test_labels)
 
-x_train = train_data
-y_train = train_labels
-x_val = dev_data
-y_val = dev_labels
-x_test = test_data
-y_test = test_labels
-x_test2 = de_test_data
-y_test2 = de_test_labels
+x_train = de_train_data
+y_train = de_train_labels
+x_val = de_dev_data
+y_val = de_dev_labels
+x_test = de_test_data
+y_test = de_test_labels
 
 # tests
 print(x_train[:3])
-print(x_test[:3])
 
 EMBEDDING_DIM = 100
 
@@ -123,18 +115,11 @@ print('trained embedding shape:', model.layers[0].get_weights()[0].shape)
 # print('test loss:', test_loss, 'test acc:', test_acc)
 gold = y_test
 predicted = model.predict(x_test).argmax(axis=1)
-gold2 = y_test2
-predicted2 = model.predict(x_test2).argmax(axis=1)
 
-print('sample en gold:', gold[:30])
-print('sample en pred:', predicted[:30])
-print('micro en:', f1_score(gold, predicted, average='micro'))
-print('macro en:', f1_score(gold, predicted, average='macro'))
-
-print('sample de gold:', gold2[:30])
-print('sample de pred:', predicted2[:30])
-print('micro de:', f1_score(gold2, predicted2, average='micro'))
-print('macro de:', f1_score(gold2, predicted2, average='macro'))
+print('sample de gold:', gold[:30])
+print('sample de pred:', predicted[:30])
+print('micro de:', f1_score(gold, predicted, average='micro'))
+print('macro de:', f1_score(gold, predicted, average='macro'))
 
 # utils.test_evaluation(gold, predicted)
 # utils.test_evaluation(gold2, predicted2)
